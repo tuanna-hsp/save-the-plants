7df9d37bd6554c073eca1e1a6a847ee9f3229356
Da Le <dalv0911@gmail.com>
2016-04-13 07:33:55 +0700

    add some button in main menu

---
+++ Assets/Scenes/Scripts/Constant.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Constant {
+    public const int map = 1;
+    public const int level = 1;
+}
---
+++ Assets/Scenes/Scripts/Main menu/MenuButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour
+{
+
+    public GameObject pressed;
+    public GameObject unpressed;
+
+    void OnMouseDown()
+    {
+        pressed.SetActive(true);
+        unpressed.SetActive(false);
+    }
+
+    void OnMouseUp()
+    {
+        pressed.SetActive(false);
+        unpressed.SetActive(true);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        pressed.SetActive(false);
+        unpressed.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
