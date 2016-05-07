ce843b2c0cf9057a0cbe410c68325634b9fe7c2b
Anh Tuấn <tuanna.hsp@gmail.com>
2016-05-07 10:14:47 +0700

    add quit dialog on main menu

--- Assets/Scripts/Main menu/MenuController.cs
+++ Assets/Scripts/Main menu/MenuController.cs
@@ -1,19 +1,41 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+    public GameObject quitPanel;
+
 	public void OnPlayButtonClick()
     {
-        Application.LoadLevel("map");
+        SceneManager.LoadScene("map");
     }
 
     public void OnHighscoreButtonClick() {
-        Application.LoadLevel("highscore");
+        SceneManager.LoadScene("highscore");
     }
 
     public void OnTutorialButtonClick()
     {
 
+    }
+
+    void Update()
+    {
+        // Esc (PC) or back button (Android)
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            quitPanel.SetActive(true);
+        }
+    }
+
+    public void ConfirmQuit()
+    {
+        Application.Quit();
+    }
+
+    public void CancelQuit()
+    {
+        quitPanel.SetActive(false);
     }
 }
